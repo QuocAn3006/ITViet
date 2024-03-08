b9f1c2d3d1f146f48a7a8566a3a9bd99ad9d8c45
Dang Phi <dangphi712@gmail.com>
2024-03-08 14:51:14 +0700

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,18 @@
+import { Icon } from '@iconify/react';
 import Section from '../components/Section';
 import { sectionCardItems } from '../constants';
+import Footer from '../components/Footer';
 
 const HomePage = () => {
 	return (
 		<>
 			<Section
 				title={'Giúp bạn quản lý dễ dàng, bán hàng hiệu quả'}
+				sectionCardItems={sectionCardItems}
+			/>
+
+			<Section
+				title={'Phi ăn cức'}
 				sectionCardItems={sectionCardItems}
 			/>
 		</>
