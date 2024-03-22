05b624c84491feed66606efd3112101a16b34bf7
Dang Phi <dangphi712@gmail.com>
2024-03-22 16:58:59 +0700

    update

--- src/Layouts/DefaultLayout.jsx
+++ src/Layouts/DefaultLayout.jsx
@@ -1,11 +1,12 @@
 import Navbar from '../components/Navbar';
-
+import Footer from '../components/Footer';
 /* eslint-disable react/prop-types */
 const DefaultLayout = ({ children }) => {
 	return (
 		<>
 			<Navbar />
 			{children}
+			<Footer/>
 		</>
 	);
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import AdminLayout from '../Layouts/AdminLayout';
+import DefaultLayout from '../Layouts/DefaultLayout';
 import AdminPage from '../pages/AdminPage';
 import CashierPage from '../pages/CashierPage';
+import ChargePage from '../pages/ChargePage';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import NotFoundPage from '../pages/NotFoundPage';
+import SolutionPage from '../pages/SolutionPage';
 
 export const routes = [
 	{
@@ -29,5 +32,15 @@ export const routes = [
 	{
 		path: '*',
 		page: NotFoundPage
-	}
+	},
+	{
+		path: '/solution',
+		page: SolutionPage,
+		layout: DefaultLayout
+	},
+	{
+		path: '/charge',
+		page: ChargePage,
+		layout: DefaultLayout
+	},
 ];
