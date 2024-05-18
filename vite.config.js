34a6387d2ac631decaf4eb569ef9b7f83284ffbd
quocan <tranan498@gmail.com>
2024-05-18 14:41:46 +0700

    update api

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
 	server: {
 		proxy: {
 			'/api': {
-				target: 'http://localhost:8000',
+				// target: 'http://localhost:8000',
+				target: 'https://itviet-backend.onrender.com',
 				changeOrigin: true,
 				rewrite: path => path.replace(/^\/api/, '')
 			}
